7e71fe6ac3ae2dc624e155531d111f1d80529b3f
GarryFCR <76639984+GarryFCR@users.noreply.github.com>
2022-02-11 01:46:07 +0530

    Point doubling and adding (#2)
    * x
    
    * point addition/doubling

--- curve-point.c
+++ curve-point.c
@@ -21,6 +21,7 @@ struct CurvePoint
 {
     struct FieldElement x;
     struct FieldElement y;
+    struct FieldElement z;
 };
 
 // FUNCTION DECLARATIONS
@@ -53,14 +54,58 @@ struct CurvePoint montgomeryLadder(struct CurvePoint *A, struct FieldElement *n)
 
 struct CurvePoint addCP(struct CurvePoint *A, struct CurvePoint *B)
 {
-    // TODO implement
-    struct CurvePoint result;
-    return result;
+
+    struct FieldElement a = subFE(&B->x, &B->z);
+    struct FieldElement b = addFE(&A->x, &A->z);
+    struct FieldElement c = addFE(&B->x, &B->z);
+    struct FieldElement d = subFE(&A->x, &B->z);
+
+    struct FieldElement e = mulFE(&a, &b); //(X2 − Z2 )(X1 + Z1)
+    struct FieldElement f = mulFE(&c, &d); //(X2 + Z2 )(X1 − Z1)
+
+    struct FieldElement g = addFE(&e, &f);
+    struct FieldElement h = subFE(&e, &f);
+
+    struct FieldElement i = mulFE(&g, &g);
+    struct FieldElement j = mulFE(&h, &h);
+
+    struct FieldElement X4 = mulFE(&A->z, &i);
+    struct FieldElement Z4 = mulFE(&A->x, &j);
+
+    struct FieldElement zero = {.upper = 0, .lower = 0};
+    struct CurvePoint sum = {.x = X4, .y = zero, .z = Z4};
+
+    return sum;
 }
 
 struct CurvePoint doubleCP(struct CurvePoint *A)
 {
-    return addCP(A, A);
+    struct FieldElement a = addFE(&A->x, &A->z);
+    struct FieldElement b = subFE(&A->x, &A->z);
+
+    a = mulFE(&a, &a);
+    b = mulFE(&b, &b);
+
+    struct FieldElement X5 = mulFE(&a, &b);
+
+    struct FieldElement four = {.upper = 0, .lower = 4};
+    struct FieldElement two = {.upper = 0, .lower = 2};
+    struct FieldElement _A = {.upper = 371610194, .lower = 1493483305};
+    struct FieldElement c = mulFE(&A->x, &A->z);
+    struct FieldElement d = addFE(&_A, &two);
+
+    //((A + 2)/4)(4X1Z1 )
+    struct FieldElement e = mulFE(&d, &c);
+    //((X1 − Z1)^2 + (A + 2)(X1Z1))
+    e = addFE(&b, &e);
+
+    struct FieldElement f = mulFE(&c, &four);
+    struct FieldElement Z5 = mulFE(&f, &e);
+
+    struct FieldElement zero = {.upper = 0, .lower = 0};
+    struct CurvePoint pdouble = {.x = X5, .y = zero, .z = Z5};
+
+    return pdouble;
 }
 
 struct CurvePoint montgomeryLadder(struct CurvePoint *A, struct FieldElement *n)
@@ -71,35 +116,26 @@ struct CurvePoint montgomeryLadder(struct CurvePoint *A, struct FieldElement *n)
     struct CurvePoint S = doubleCP(A);
 
     //Get MSB
-    uint31 temp = n->upper;
+    unsigned long temp = convertToUnsignedLong(n);
     int len = (int)(log(temp) / log(2)) + 1;
     int pos = (1 << (len - 1));
 
-    for (int k = 0; k < 2; k++)
+    for (int i = len; i > 0; i--)
     {
-
-        for (int i = len; i > 0; i--)
+        if ((pos & temp) == pos)
         {
-            if ((pos & temp) == pos)
-            {
-                //"1"
-                S = addCP(&S, &R);
-                R = doubleCP(&R);
-            }
-            else
-            {
-                //"0"
-                R = addCP(&S, &R);
-                S = doubleCP(&R);
-            }
-            temp = temp << 1;
+            //"1"
+            S = addCP(&S, &R);
+            R = doubleCP(&R);
         }
-
-        temp = n->lower;
-        len = (int)(log(temp) / log(2)) + 1;
-        pos = (1 << (len - 1));
+        else
+        {
+            //"0"
+            R = addCP(&S, &R);
+            S = doubleCP(&R);
+        }
+        temp = temp << 1;
     }
 
-    struct CurvePoint result;
-    return result;
+    return R;
 }
