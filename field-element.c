92b81afbe009aabff9bc20264cbd539e2a6d1b04
GarryFCR <garryranee@gmail.com>
2022-02-11 01:31:19 +0530

    point addition/doubling

--- field-element.c
+++ field-element.c
@@ -237,3 +237,26 @@ void printBinaryFE(struct FieldElement *a)
 {
     printBinaryFEWithLabel("", a);
 }
+
+struct FieldElement power(struct FieldElement *a, struct FieldElement *b, struct FieldElement *p)
+{
+    int res = 1; // Initialize result
+
+    x = x % p; // Update x if it is more than or
+               // equal to p
+
+    if (x == 0)
+        return 0; // In case x is divisible by p;
+
+    while (y > 0)
+    {
+        // If y is odd, multiply x with result
+        if (y & 1)
+            res = (res * x) % p;
+
+        // y must be even now
+        y = y >> 1; // y = y/2
+        x = (x * x) % p;
+    }
+    return res;
+}
